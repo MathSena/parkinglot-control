0ac9b8efdd8faa2cb224cec2232f287dca1e3577
Matheus Sena <MathSena07@hotmail.com>
2022-08-02 20:26:31 -0300

    Finalizando API

---
+++ src/test/java/com/mathsena/parkinglotcontrol/ParkinglotControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.parkinglotcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkinglotControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
